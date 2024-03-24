85e541f767b585ae089123afd898efd08c5be96a
Durisetti Srinikhil <47129168+Srinikhil@users.noreply.github.com>
2024-03-24 17:55:00 -0400

    Time: 18 ms (64.72%), Space: 11.9 MB (53.63%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,13 +1,41 @@
 class Solution(object):
-    def findSets(self, idx, combSet, reqSet, nums):
-        if idx >= len(nums):
-            reqSet.append(tuple(combSet[:]))
-            return
+    # Recursion using additional set
+#     def findSets(self, idx, combSet, reqSet, nums):
+#         if idx >= len(nums):
+#             reqSet.append(tuple(combSet[:]))
+#             return
         
-        combSet.append(nums[idx])
-        self.findSets(idx+1, combSet, reqSet, nums)
-        combSet.pop()
-        self.findSets(idx+1, combSet, reqSet, nums)
+#         combSet.append(nums[idx])
+#         self.findSets(idx+1, combSet, reqSet, nums)
+#         combSet.pop()
+#         self.findSets(idx+1, combSet, reqSet, nums)
+    
+    
+    
+#     def subsetsWithDup(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[List[int]]
+#         """
+        
+#         idx = 0
+#         reqSet = []
+#         combSet = []
+#         nums.sort()
+#         self.findSets(idx, combSet, reqSet, nums)
+#         # reqSet = tuple(reqSet)
+#         return set(reqSet)
+
+    def findSets(self, idx, combSet, reqSet, nums):
+        reqSet.append(combSet[:])
+        
+        for i in range(idx, len(nums)):
+            if i>idx and nums[i] == nums[i-1]:
+                continue
+                
+            combSet.append(nums[i])
+            self.findSets(i+1, combSet, reqSet, nums)
+            combSet.pop()
     
     
     
@@ -16,14 +44,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
         idx = 0
         reqSet = []
         combSet = []
         nums.sort()
         self.findSets(idx, combSet, reqSet, nums)
-        # reqSet = tuple(reqSet)
-        return set(reqSet)
+        
+        
+        return reqSet
         
         
         
