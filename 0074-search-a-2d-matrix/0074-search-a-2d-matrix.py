c024e108e11da8a4ac4173b899856e55439d0d2b
Durisetti Srinikhil <47129168+Srinikhil@users.noreply.github.com>
2024-05-06 19:58:36 -0400

    Time: 27 ms (39.94%), Space: 12.1 MB (17.87%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -9,19 +9,25 @@ class Solution(object):
         low = 0
         high = len(matrix[i]) - 1
         
-        while i<len(matrix) and low <= high:
-        
+        while i<len(matrix)-1:
             if target > matrix[i][high]:
                 i += 1
             else:
+                break
+                
+        while low <= high:
+            mid = (low + high) // 2
+            if target == matrix[i][mid]:
+                return True
+            elif target < matrix[i][mid]:
+                high = mid - 1
+            else:
+                low = mid+1
+        
+        
+            
                 # mid = low + ((low - high)) //2
-                mid = (low + high) // 2
-                if target == matrix[i][mid]:
-                    return True
-                elif target < matrix[i][mid]:
-                    high = mid - 1
-                else:
-                    low = mid+1
+                
         return False
                 
                 
