02175dfaa4f6d1f46b39d14a13e72a122591cee7
Durisetti Srinikhil <47129168+Srinikhil@users.noreply.github.com>
2024-05-06 21:30:24 -0400

    Time: 7 ms (97.34%), Space: 11.7 MB (56.94%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,24 +4,45 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+#         parStack = []
+        
+#         for ch in s:
+#             if ch == '(' or ch == '[' or ch == '{':
+#                 parStack.append(ch)
+#             elif ch == ')' and len(parStack) != 0:
+#                 if parStack.pop() != '(':
+#                     return False
+#             elif ch == ']' and len(parStack) != 0:
+#                 if parStack.pop() != '[':
+#                     return False
+#             elif ch == '}' and len(parStack) != 0:
+#                 if parStack.pop() != '{':
+#                     return False
+#             else:
+#                 return False
+            
+#         if len(parStack) != 0:
+#             return False
+        
+#         return True
+
+
         parStack = []
+        bracketsDict = {
+            '(':')',
+            '[':']',
+            '{':'}'
+        }
         
         for ch in s:
-            if ch == '(' or ch == '[' or ch == '{':
+            if ch in bracketsDict.keys():
                 parStack.append(ch)
-            elif ch == ')' and len(parStack) != 0:
-                if parStack.pop() != '(':
-                    return False
-            elif ch == ']' and len(parStack) != 0:
-                if parStack.pop() != '[':
-                    return False
-            elif ch == '}' and len(parStack) != 0:
-                if parStack.pop() != '{':
-                    return False
-            else:
+            elif len(parStack) == 0 or ch != bracketsDict[parStack.pop()]:
                 return False
             
-        if len(parStack) != 0:
-            return False
+        # if len(parStack) == 0:
+        #     return True
+        # else:
+        #     return False
         
-        return True
+        return len(parStack) == 0
