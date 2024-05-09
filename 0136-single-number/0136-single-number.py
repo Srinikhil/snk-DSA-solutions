0d6101b97326931a1df28acb0299fc0b4088fb67
Durisetti Srinikhil <47129168+Srinikhil@users.noreply.github.com>
2024-05-08 20:43:39 -0400

    Time: 93 ms (72.20%), Space: 13.7 MB (76.42%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -6,15 +6,26 @@ class Solution(object):
         """
         #TC - O(n), SC - O(n)
         
-        freqDict = {}
-        for n in nums:
-            if n in freqDict:
-                freqDict[n] += 1
-            else:
-                freqDict[n] = 1
+#         freqDict = {}
+#         for n in nums:
+#             if n in freqDict:
+#                 freqDict[n] += 1
+#             else:
+#                 freqDict[n] = 1
 
-        for i in freqDict:
-            if freqDict[i] < 2:
-                return i
+#         for i in freqDict:
+#             if freqDict[i] < 2:
+#                 return i
+
+        # But only constant extra space can be used.
+
+        # using XOR this can be done, all elements which appeared twice cancelout leaving the required one.
+        
+        r = 0
+        for n in nums:
+            r = r ^ n
+            
+        return r
+    
         
         
