c687db3de954b0640bfaab9771572bcb0f8cc500
Durisetti Srinikhil <47129168+Srinikhil@users.noreply.github.com>
2024-05-10 17:07:15 -0400

    Time: 55 ms (48.37%), Space: 15.7 MB (12.91%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,11 +1,13 @@
 class Solution(object):
-    def countOnes(self, num):
-        setBitCount = 0
-        while num > 0:
-            setBitCount += 1
-            num = num & (num - 1)
+    
+    # Bruteforce approach
+#     def countOnes(self, num):
+#         setBitCount = 0
+#         while num > 0:
+#             setBitCount += 1
+#             num = num & (num - 1)
             
-        return setBitCount
+#         return setBitCount
     
     
     def countBits(self, n):
@@ -14,9 +16,22 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        bitCounts = []
-        for num in range(n+1):
-            bitCounts.append(self.countOnes(num))
+#         bitCounts = []
+#         for num in range(n+1):
+#             bitCounts.append(self.countOnes(num))
             
-        return bitCounts
+#         return bitCounts
+
+
+        bitCounts = [0]
+        latest2n = 0
         
+        for i in range(1, n+1):
+            if i and (not (i & (i-1))):
+                bitCounts.append(1)
+                latest2n = i
+            else:
+                bitCounts.append(bitCounts[latest2n] + bitCounts[i - latest2n])
+
+                
+        return bitCounts
