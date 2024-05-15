b39828becca82bf58cacfde8698a0b5008498887
Durisetti Srinikhil <47129168+Srinikhil@users.noreply.github.com>
2024-05-14 20:36:19 -0400

    Time: 56 ms (12.25%), Space: 11.6 MB (37.16%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -16,33 +16,38 @@ class Solution(object):
 #             j -= 1
             
 #         return True
+
+        
+        # reverse string and compare
+        # if x < 0:
+        #     return False
+        # else:
+        #     x = str(x)
+        #     if x != x[::-1]:
+        #         return False
+        #     return True
+        
         
     # Approach - 2
-#         arr = []
-
-#         if x < 0:
-#             return False
-#         else:
-#             while x > 0:
-#                 arr.append(x%10)
-#                 x = x // 10
-                
-#             i = 0
-#             j = len(arr) - 1
-#             while i <= j:
-#                 if arr[i] != arr[j]:
-#                     return False
-#                 i += 1
-#                 j -= 1
-                
-#             return True
+        arr = []
 
         if x < 0:
             return False
         else:
-            x = str(x)
-            if x != x[::-1]:
-                return False
+            while x > 0:
+                arr.append(x%10)
+                x = x // 10
+                
+            i = 0
+            j = len(arr) - 1
+            while i <= j:
+                if arr[i] != arr[j]:
+                    return False
+                i += 1
+                j -= 1
+                
             return True
+
+
     
         
