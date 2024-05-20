c6bfb81d6c9f8ba1d413371555c1d6c1c334cad2
Durisetti Srinikhil <47129168+Srinikhil@users.noreply.github.com>
2024-05-19 20:43:58 -0400

    Time: 3 ms (99.76%), Space: 11.5 MB (96.15%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,18 +1,18 @@
 class Solution(object):
     
-    def rf(self, idx, nums, dp):
-        if idx == 0:
-            return nums[idx]
-        if idx < 0:
-            return 0
-        if dp[idx] != -1:
-            return dp[idx]
+#     def rf(self, idx, nums, dp):
+#         if idx == 0:
+#             return nums[idx]
+#         if idx < 0:
+#             return 0
+#         if dp[idx] != -1:
+#             return dp[idx]
         
-        pick = nums[idx] + self.rf(idx-2, nums, dp)
-        notPick = 0 + self.rf(idx-1, nums, dp)
+#         pick = nums[idx] + self.rf(idx-2, nums, dp)
+#         notPick = 0 + self.rf(idx-1, nums, dp)
         
-        dp[idx] = max(pick, notPick)
-        return dp[idx]
+#         dp[idx] = max(pick, notPick)
+#         return dp[idx]
     
     def rob(self, nums):
         """
@@ -24,8 +24,26 @@ class Solution(object):
         # return self.rf(n-1, nums)
         
         
-        
-        dp = [-1]*len(nums)
+        # Memoization approach
+#         dp = [-1]*len(nums)
 
+#         n = len(nums)
+#         return self.rf(n-1, nums, dp)
+
+
+
+        # Tabulation approach
+        dp = [0]*len(nums)
+        dp[0] = nums[0]
+        
         n = len(nums)
-        return self.rf(n-1, nums, dp)
+        for i in range(1, n):
+            pick = nums[i]
+            if i > 0:
+                pick += dp[i-2]
+            
+            notPick = 0 + dp[i-1]
+            
+            dp[i] = max(pick, notPick)
+        
+        return dp[n-1]
