0700c3c148a1ec847424598c4886d2f328c6d607
Durisetti Srinikhil <47129168+Srinikhil@users.noreply.github.com>
2024-05-22 21:23:28 -0400

    Time: 14 ms (66.70%), Space: 11.4 MB (99.79%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,33 +1,34 @@
 class Solution(object):
     # Space Optimized approach
-#     def maxRob(self, nums):
-#         prev2, prev = 0, nums[0]
+    def maxRob(self, nums):
+        prev2, prev = 0, nums[0]
         
-#         for i in range(1, len(nums)):
-#             pick = nums[i]
-#             if i > 1:
-#                 pick += prev2
-#             notPick = 0 + prev
+        for i in range(1, len(nums)):
+            pick = nums[i]
+            if i > 1:
+                pick += prev2
+            notPick = 0 + prev
             
-#             prev2 = prev
-#             prev = max(pick, notPick)
+            prev2 = prev
+            prev = max(pick, notPick)
             
-#         return prev
+        return prev
 
-    def maxRob(self, nums, idx, dp):
-        if idx == 0:
-            return nums[0]
-        if idx < 0:
-            return 0
-        if dp[idx] != -1:
-            return dp[idx]
+    # Memoization
+#     def maxRob(self, nums, idx, dp):
+#         if idx == 0:
+#             return nums[0]
+#         if idx < 0:
+#             return 0
+#         if dp[idx] != -1:
+#             return dp[idx]
         
-        pick = nums[idx] + self.maxRob(nums, idx-2, dp)
-        notPick = 0 + self.maxRob(nums, idx-1, dp)
+#         pick = nums[idx] + self.maxRob(nums, idx-2, dp)
+#         notPick = 0 + self.maxRob(nums, idx-1, dp)
             
-        dp[idx] = max(pick, notPick)
+#         dp[idx] = max(pick, notPick)
         
-        return dp[idx]
+#         return dp[idx]
             
     
     def rob(self, nums):
@@ -38,10 +39,12 @@ class Solution(object):
         n = len(nums)
         if n == 1:
             return nums[0]
-        # return max(self.maxRob(nums[:n-1]), self.maxRob(nums[1:]))
-        dp1 = [-1]*len(nums)
-        dp2 = [-1]*len(nums)
-        return max(self.maxRob(nums[:n-1], n-2, dp1), self.maxRob(nums[1:], n-2, dp2))
+        return max(self.maxRob(nums[:n-1]), self.maxRob(nums[1:]))
+        
+        # Memoization Approach
+        # dp1 = [-1]*len(nums)
+        # dp2 = [-1]*len(nums)
+        # return max(self.maxRob(nums[:n-1], n-2, dp1), self.maxRob(nums[1:], n-2, dp2))
         
         
         
