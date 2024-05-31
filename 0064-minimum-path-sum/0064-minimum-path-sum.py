d5004e60fd6d384e02c672df6922f4b832e49281
Durisetti Srinikhil <47129168+Srinikhil@users.noreply.github.com>
2024-05-31 14:27:42 -0400

    Time: 71 ms (27.59%), Space: 13 MB (70.18%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,19 +1,20 @@
 import sys
 
 class Solution(object):
-    def pathFun(self, i, j, arr, dp):
-        if i == 0 and j == 0:
-            return arr[i][j]
-        if i < 0 or j < 0:
-            return sys.maxint
-        if dp[i][j] != -1:
-            return dp[i][j]
+    # Memoization Approach
+#     def pathFun(self, i, j, arr, dp):
+#         if i == 0 and j == 0:
+#             return arr[i][j]
+#         if i < 0 or j < 0:
+#             return sys.maxint
+#         if dp[i][j] != -1:
+#             return dp[i][j]
         
-        up = arr[i][j] + self.pathFun(i-1, j, arr, dp)
-        left = arr[i][j] + self.pathFun(i, j-1, arr, dp)
+#         up = arr[i][j] + self.pathFun(i-1, j, arr, dp)
+#         left = arr[i][j] + self.pathFun(i, j-1, arr, dp)
         
-        dp[i][j] = min(up, left)
-        return dp[i][j]
+#         dp[i][j] = min(up, left)
+#         return dp[i][j]
     
     
     
@@ -25,6 +26,25 @@ class Solution(object):
         m = len(grid)
         n = len(grid[0])
         
-        dp = [[-1]*n for _ in range(m)]
+        # Memoization Approach
+        # dp = [[-1]*n for _ in range(m)]
+        # return self.pathFun(m-1, n-1, grid, dp)
         
-        return self.pathFun(m-1, n-1, grid, dp)
+        # Tabulation Approach
+        dp = [[0]*n for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[i][j]
+                else:
+                    up, left = sys.maxint, sys.maxint
+                    if i>0:
+                        up = grid[i][j] + dp[i-1][j]
+                    if j>0:
+                        left = grid[i][j] + dp[i][j-1]
+                    
+                    dp[i][j] = min(up,left)
+                    # print("i", i, "j", j, "dp-->", dp)
+                    
+        return dp[m-1][n-1]
