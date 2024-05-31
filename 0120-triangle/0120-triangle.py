1de128c8be830d58732be8b4357371fd6d2d9b0b
Durisetti Srinikhil <47129168+Srinikhil@users.noreply.github.com>
2024-05-31 15:57:55 -0400

    Time: 46 ms (31.49%), Space: 12.7 MB (37.14%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,17 +1,17 @@
 class Solution(object):
-    # Memoization Approach TC - O(N), SC - O(N)+O(N*N)
-    def pathFun(self, i, j, triangle, dp):
-        n = len(triangle)
-        if i == n-1:
-            return triangle[i][j]
-        if dp[i][j] != -1:
-            return dp[i][j]
+    # Memoization Approach TC - O(N*N), SC - O(N)+O(N*N)
+#     def pathFun(self, i, j, triangle, dp):
+#         n = len(triangle)
+#         if i == n-1:
+#             return triangle[i][j]
+#         if dp[i][j] != -1:
+#             return dp[i][j]
         
-        down = triangle[i][j] + self.pathFun(i+1, j, triangle, dp)
-        diag = triangle[i][j] + self.pathFun(i+1, j+1, triangle, dp)
+#         down = triangle[i][j] + self.pathFun(i+1, j, triangle, dp)
+#         diag = triangle[i][j] + self.pathFun(i+1, j+1, triangle, dp)
         
-        dp[i][j] = min(down, diag)
-        return dp[i][j]
+#         dp[i][j] = min(down, diag)
+#         return dp[i][j]
     
     
     
@@ -20,9 +20,26 @@ class Solution(object):
         :type triangle: List[List[int]]
         :rtype: int
         """
-        n = len(triangle)
-        dp = [[-1]*n for _ in range(n)]
+        # Memoization Approach
+#         n = len(triangle)
+#         dp = [[-1]*n for _ in range(n)]
         
-        return self.pathFun(0, 0, triangle, dp)
+#         return self.pathFun(0, 0, triangle, dp)
+
+        # Tabulation TC - O(N*N), SC - O(N*N)
+        n = len(triangle)
+        dp = [[0]*n for _ in range(n)]
+        
+        # Base Case
+        for j in range(n):
+            dp[n-1][j] = triangle[n-1][j]
+            
+        for i in range(n-2, -1, -1):
+            for j in range(i, -1, -1):
+                down = triangle[i][j] + dp[i+1][j]
+                diag = triangle[i][j] + dp[i+1][j+1]
+                dp[i][j] = min(down, diag)
+                
+        return dp[0][0]
         
         
