07f4575151aad09f4e670a94edd076b7a89bdc03
Durisetti Srinikhil <47129168+Srinikhil@users.noreply.github.com>
2024-06-03 21:35:09 -0400

    Time: 100 ms (62.68%), Space: 16.5 MB (90.21%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -26,38 +26,15 @@ class Solution(object):
             
             
         # Optimized Approach TC - O(nlogn) + O(n)
-#         n = len(intervals)
-#         for i in range(n):
-#             # start = intervals[i][0]
-#             # end = intervals[i][1]
-            
-#             if mergedIntervals == []:
-#                 mergedIntervals.append(intervals[i])
-#             if intervals[i][0] <= mergedIntervals[-1][1]:
-#                 mergedIntervals[-1][1] = max(mergedIntervals[-1][1], intervals[i][1])
-#             else:
-#                 mergedIntervals.append(intervals[i])
-            
-            
-
-            
-        # intervals.sort()
         n = len(intervals)
-        temp = n
+        for i in range(n):
+            if mergedIntervals == []:
+                mergedIntervals.append(intervals[i])
+            if intervals[i][0] <= mergedIntervals[-1][1]:
+                mergedIntervals[-1][1] = max(mergedIntervals[-1][1], intervals[i][1])
+            else:
+                mergedIntervals.append(intervals[i])
+            
+            
         
-        #for i in range(len(intervals) - 3):
-        i = 0
-        while i<temp-1:
-            if intervals[i][1] >= intervals[i+1][0] and intervals[i][0] <= intervals[i+1][0]:
-                intervals[i] = [intervals[i][0], max([intervals[i][1], intervals[i+1][1]])]
-                del intervals[i+1]
-            if temp == len(intervals):
-                i += 1
-            temp = len(intervals)
-        print(intervals)
-        
-        return intervals
-
-
-        
-        # return mergedIntervals
+        return mergedIntervals
